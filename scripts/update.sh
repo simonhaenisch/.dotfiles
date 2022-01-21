3c77e65ef3bc36c3d2b49d14a44acaa6955b149d
simonhaenisch <simonhaenisch@users.noreply.github.com>
2022-01-21 21:08:03 +0100

    fix update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env zsh
 
 # brew and casks
-/usr/local/bin/brew upgrade
-/usr/local/bin/brew cleanup -s
+brew upgrade
+brew cleanup -s
 
 # npm and global packages
-/usr/local/bin/npm install -g npm@latest
-/usr/local/bin/npm update -g
+npm install -g npm@latest
+npm update -g
